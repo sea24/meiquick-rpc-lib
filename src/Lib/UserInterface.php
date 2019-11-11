ecffc3c83e62ff4aaa3cb02c977fc7687367d673
Sea <2602672132@qq.com>
2019-11-11 15:09:32 +0800

    增加形参传递

--- src/Lib/UserInterface.php
+++ src/Lib/UserInterface.php
@@ -1,6 +1,8 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace MeiQuick\Rpc\Lib;
+
+use Swoft\Http\Message\Request;
 
 /**
  * Class UserInterface
@@ -10,94 +12,114 @@ namespace MeiQuick\Rpc\Lib;
 interface UserInterface
 {
     /**
+     * @param array $param
      * @return array
      */
-    public function registerByEmail(): array;
+    public function registerByEmail(array $param): array;
 
     /**
+     * @param array $param
      * @return array
      */
-    public function registerByMobile(): array;
+    public function registerByMobile(array $param): array;
 
     /**
+     * @param array $param
      * @return array
      */
-    public function login(): array;
+    public function login(array $param): array;
 
     /**
+     * @param array $param
      * @return array
      */
-    public function resetPassword(): array;
+    public function resetPassword(array $param): array;
 
     /**
+     * @param array $param
      * @return array
      */
-    public function getEmailVerifyCode(): array;
-
+    public function getEmailVerifyCode(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function getMobileVerifyCode(): array;
-
+    public function getMobileVerifyCode(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function getVerifyCode(): array;
-
-    //需要登录功能
-    /**
+    public function getVerifyCode(array $param): array;
+    
+    
+    //需要验证登录
+        /**
+     * @param array $param
      * @return array
      */
-    public function infoIndex(): array;
+    public function infoIndex(array $param): array;
 
     /**
+     * @param array $param
      * @return array
      */
-    public function editEmail(): array;
-
+    public function editEmail(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function editMobile(): array;
-
+    public function editMobile(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function bindEmail(): array;
-
+    public function bindEmail(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function bindMobile(): array;
-
+    public function bindMobile(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function editUserInfo(): array;
-
+    public function editUserInfo(array $param): array;
+    
     /**
+     * @param array $param
      * @return array
      */
-    public function editPassword(): array;
+    public function editPassword(array $param): array;
 
     /**
+     * 获取防止重复提交token
+     * @param array $param
      * @return array
      */
-    public function getFormToken(): array;
+    public function getFormToken(array $param): array;
 
     /**
+     * 新增收货地址
      * @param array $param
      * @return array
      */
     public function addReceiveAddress(array $param): array;
 
     /**
+     * 修改收货地址
      * @param array $param
      * @return array
      */
     public function editReceiveAddress(array $param): array;
 
     /**
+     * 删除收货地址
      * @param array $param
      * @return array
      */
