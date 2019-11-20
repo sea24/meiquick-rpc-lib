3c80c4a3b1b78712e4c8783f545bcffdb78ab275
Sea <2602672132@qq.com>
2019-11-20 13:33:56 +0800

    增加会员接口

--- src/Lib/UserInterface.php
+++ src/Lib/UserInterface.php
@@ -2,8 +2,6 @@
 
 namespace MeiQuick\Rpc\Lib;
 
-use Swoft\Http\Message\Request;
-
 /**
  * Class UserInterface
  *
@@ -12,129 +10,136 @@ use Swoft\Http\Message\Request;
 interface UserInterface
 {
     /**
-     * @param array $param
      * @return array
      */
     public function registerByEmail(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function registerByMobile(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function login(array $param): array;
 
     /**
-     * @param array $param
+     * 退出登录
      * @return array
      */
     public function logout(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function resetPassword(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function getEmailVerifyCode(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function getMobileVerifyCode(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function getVerifyCode(array $param): array;
 
-
-    //需要验证登录
-        /**
-     * @param array $param
+    //需要登录功能
+    /**
      * @return array
      */
     public function infoIndex(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function editEmail(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function editMobile(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function bindEmail(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function bindMobile(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function editUserInfo(array $param): array;
 
     /**
-     * @param array $param
      * @return array
      */
     public function editPassword(array $param): array;
 
     /**
-     * 获取防止重复提交token
-     * @param array $param
      * @return array
      */
     public function getFormToken(array $param): array;
 
     /**
-     * 新增收货地址
      * @param array $param
      * @return array
      */
     public function addReceiveAddress(array $param): array;
 
     /**
-     * 修改收货地址
      * @param array $param
      * @return array
      */
     public function editReceiveAddress(array $param): array;
 
     /**
-     * 删除收货地址
      * @param array $param
      * @return array
      */
     public function deleteReceiveAddress(array $param): array;
 
     /**
-     * 获取收货地址
      * @param array $param
      * @return array
      */
     public function getReceiveAddress(array $param): array;
+
+    /**
+     * 获取地区下拉框
+     * @param array $param
+     * @return array
+     */
+    public function getUsdDistrict(array $param): array;
+
+    /**
+     * 获取默认地址
+     * @param array $param
+     * @return array
+     */
+    public function getDefaultAddress(array $param): array;
+
+    /**
+     * 根据收件人id获取收件人信息
+     * @param array $param
+     * @return array
+     */
+    public function getReceiveAddressById(array $param): array;
+
+    /**
+     * 设为默认地址
+     * @param array $param
+     * @return array
+     */
+    public function setDefaultAddress(array $param): array;
 }
